18e4def8eb264cd190b9d30817a40ed534b17e00
IagoACPereira <iago0307@gmail.com>
2024-09-10 00:52:02 -0300

    feat: :sparkles: add: aula4: Customizando erros

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
-import express from "express";
+import 'express-async-errors';
+import express, { Request, Response } from "express";
 import router from "./routes";
 import "reflect-metadata";
 import { AppDataSource } from "./config/dataSource";
+import { erroMiddleware } from "./middlewares/erro";
 const app = express();
 app.use(express.json());
 router(app);
+
+app.get('/teste', (req: Request, res: Response) => {
+  throw new Error('Erro Teste');
+})
+
+app.use(erroMiddleware);
 
 AppDataSource.initialize()
   .then(() => {
